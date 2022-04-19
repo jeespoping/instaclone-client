299e22a789725748146e387aa57d8cbc7c63cc5d
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-04-19 12:26:09 -0500

    Conectando la app con el servidor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "semantic-ui-react";
+import { ApolloProvider } from "@apollo/client";
+import client from "./config/apollo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Button primary>Primary</Button>
+        <Button secondary>Secondary</Button>
+      </div>
+    </ApolloProvider>
   );
 }
 
