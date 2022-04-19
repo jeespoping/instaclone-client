fd8e31b7971a82b69be5919642ea5ee92917681c
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-04-19 13:43:31 -0500

    control de datos del formulario

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,30 +1,52 @@
 import React from "react";
 import { Form, Button } from "semantic-ui-react";
+import { useFormik } from "formik";
 import "./RegisterForm.scss";
 
 export default function RegisterForm({ setShowLogin }) {
-  const onSubmit = () => {
-    console.log("Formulario enviado");
-  };
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: null,
+    onSubmit: (formValue) => {
+      console.log(formValue);
+    },
+  });
 
   return (
     <>
       <h2 className="register-form-title">
         Registrate para ver fotos y videos de tus amigos.
       </h2>
-      <Form className="register-form" onSubmit={onSubmit}>
-        <Form.Input type="text" placeholder="Nombre y apellidos" name="name" />
+      <Form className="register-form" onSubmit={formik.handleSubmit}>
+        <Form.Input
+          onChange={formik.handleChange}
+          type="text"
+          placeholder="Nombre y apellidos"
+          name="name"
+        />
         <Form.Input
           type="text"
           placeholder="Nombre de usuario"
           name="username"
+          onChange={formik.handleChange}
         />
-        <Form.Input type="text" placeholder="Correo electronico" name="email" />
-        <Form.Input type="password" placeholder="Contraseña" name="password" />
+        <Form.Input
+          onChange={formik.handleChange}
+          type="text"
+          placeholder="Correo electronico"
+          name="email"
+        />
+        <Form.Input
+          onChange={formik.handleChange}
+          type="password"
+          placeholder="Contraseña"
+          name="password"
+        />
         <Form.Input
           type="password"
           placeholder="Repetir contraseña"
           name="repeatPassword"
+          onChange={formik.handleChange}
         />
 
         <Button type="submit" className="btn-submit">
@@ -33,4 +55,14 @@ export default function RegisterForm({ setShowLogin }) {
       </Form>
     </>
   );
+}
+
+function initialValues() {
+  return {
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+    repeatPassword: "",
+  };
 }
