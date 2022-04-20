df4fcc55f97ed64dffe68e89b4522c7aafeef052
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-04-20 12:05:17 -0500

    Crando todas las paginas de nuestra aplicación

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,6 @@
 import React from "react";
-import useAuth from "../../hooks/useAuth";
+import "./Home.scss";
 
 export default function Home() {
-  const auth = useAuth();
-  console.log(auth);
   return <div>Home</div>;
 }
