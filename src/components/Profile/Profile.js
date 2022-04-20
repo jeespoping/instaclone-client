db77a05bd9e49b60acaee68fca7f794f1470e096
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-04-20 16:30:03 -0500

    Estructura del perfil de usuario

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Grid, Image } from "semantic-ui-react";
 import { useQuery } from "@apollo/client";
 import "./Profile.scss";
+import ImageNoFound from "../../assets/png/avatar.png";
 import { GET_USER } from "../../gql/user";
 
 export default function Profile({ username }) {
@@ -16,5 +18,28 @@ export default function Profile({ username }) {
 
   const { getUser } = data;
 
-  return <div>Profile</div>;
+  return (
+    <>
+      <Grid className="profile">
+        <Grid.Column width={5} className="profile__left">
+          <Image src={ImageNoFound} avatar />
+        </Grid.Column>
+        <Grid.Column width={10} className="profile__right">
+          <div>HeaderPrfile</div>
+          <div>Followers</div>
+          <div className="other">
+            <p className="name">{getUser.name}</p>
+            {getUser.siteWeb && (
+              <a href={getUser.siteWeb} className="siteWeb" target="_blank">
+                {getUser.siteWeb}
+              </a>
+            )}
+            {getUser.description && (
+              <p className="description">{getUser.description}</p>
+            )}
+          </div>
+        </Grid.Column>
+      </Grid>
+    </>
+  );
 }
