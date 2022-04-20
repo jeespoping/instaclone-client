46ae21ed5337c6c805dc5f920757fb7cf92eff98
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-04-20 10:41:45 -0500

    añadiendo formik

--- src/components/Auth/LoginForm/LoginForm.js
+++ src/components/Auth/LoginForm/LoginForm.js
@@ -1,16 +1,43 @@
 import React from "react";
 import { Form, Button } from "semantic-ui-react";
+import { useFormik } from "formik";
 import "./LoginForm.scss";
 
 export default function LoginForm() {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: null,
+    onSubmit: (formData) => {
+      console.log(formData);
+    },
+  });
   return (
-    <Form className="login-form">
+    <Form onSubmit={formik.handleSubmit} className="login-form">
       <h2>Entra para ver fotos y videos de tus amigos</h2>
-      <Form.Input type="text" placeholder="Correo electronico" name="email" />
-      <Form.Input type="password" placeholder="Contraseña" name="password" />
+      <Form.Input
+        onChange={formik.handleChange}
+        type="text"
+        placeholder="Correo electronico"
+        name="email"
+        value={formik.values.email}
+      />
+      <Form.Input
+        onChange={formik.handleChange}
+        type="password"
+        placeholder="Contraseña"
+        name="password"
+        value={formik.values.password}
+      />
       <Button type="submit" className="btn-submit">
         Iniciar sesión
       </Button>
     </Form>
   );
+}
+
+function initialValues() {
+  return {
+    email: "",
+    password: "",
+  };
 }
