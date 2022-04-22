f7a4bbec566cd655787da5cc466d0d4d2eec5fb6
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-04-22 13:08:44 -0500

    Añadiendo un spinner y cerrando el modal al completar la subida del avatar

--- src/components/User/AvatarForm/AvatarForm.js
+++ src/components/User/AvatarForm/AvatarForm.js
@@ -1,5 +1,6 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { Button } from "semantic-ui-react";
+import { toast } from "react-toastify";
 import { useDropzone } from "react-dropzone";
 import { useMutation } from "@apollo/client";
 import { UPDATE_AVATAR } from "../../../gql/user";
@@ -7,15 +8,25 @@ import "./AvatarForm.scss";
 
 export default function AvatarForm({ setShowModal }) {
   const [updateAvatar] = useMutation(UPDATE_AVATAR);
+  const [loading, setLoading] = useState(false);
 
   const onDrop = useCallback(async (acceptedFile) => {
     const file = acceptedFile[0];
 
     try {
+      setLoading(true);
       const result = await updateAvatar({ variables: { file } });
-      console.log(result);
+      const { data } = result;
+      if (!data.updateAvatar.status) {
+        toast.warning("Error al actualizar el avatar");
+        setLoading(false);
+      } else {
+        setLoading(false);
+        setShowModal(false);
+      }
     } catch (error) {
       console.log(error);
+      setLoading(false);
     }
   }, []);
 
@@ -28,7 +39,9 @@ export default function AvatarForm({ setShowModal }) {
 
   return (
     <div className="avatar-form">
-      <Button {...getRootProps()}>Cargar una foto</Button>
+      <Button {...getRootProps()} loading={loading}>
+        Cargar una foto
+      </Button>
       <Button>Eliminar foto actual</Button>
       <Button onClick={() => setShowModal(false)}>Cancelar</Button>
       <input {...getInputProps()} />
