c1a79681089a11288709367860d8fe842a643812
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-04-22 15:29:11 -0500

    Eliminando el avatar con el boton

--- src/components/User/AvatarForm/AvatarForm.js
+++ src/components/User/AvatarForm/AvatarForm.js
@@ -3,7 +3,7 @@ import { Button } from "semantic-ui-react";
 import { toast } from "react-toastify";
 import { useDropzone } from "react-dropzone";
 import { useMutation } from "@apollo/client";
-import { UPDATE_AVATAR, GET_USER } from "../../../gql/user";
+import { UPDATE_AVATAR, GET_USER, DELETE_AVATAR } from "../../../gql/user";
 import "./AvatarForm.scss";
 
 export default function AvatarForm({ setShowModal, auth }) {
@@ -25,6 +25,30 @@ export default function AvatarForm({ setShowModal, auth }) {
           getUser: {
             ...getUser,
             avatar: updateAvatar.urlAvatar,
+          },
+        },
+      });
+    },
+  });
+
+  const [deleteAvatar] = useMutation(DELETE_AVATAR, {
+    update(cache) {
+      const { getUser } = cache.readQuery({
+        query: GET_USER,
+        variables: {
+          username: auth.username,
+        },
+      });
+
+      cache.writeQuery({
+        query: GET_USER,
+        variables: {
+          username: auth.username,
+        },
+        data: {
+          getUser: {
+            ...getUser,
+            avatar: "",
           },
         },
       });
@@ -60,12 +84,27 @@ export default function AvatarForm({ setShowModal, auth }) {
     onDrop,
   });
 
+  const onDeleteAvatar = async () => {
+    try {
+      const result = await deleteAvatar();
+      const { data } = result;
+
+      if (!data.deleteAvatar) {
+        toast.warning("Error al borrar el avatar");
+      } else {
+        setShowModal(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="avatar-form">
       <Button {...getRootProps()} loading={loading}>
         Cargar una foto
       </Button>
-      <Button>Eliminar foto actual</Button>
+      <Button onClick={onDeleteAvatar}>Eliminar foto actual</Button>
       <Button onClick={() => setShowModal(false)}>Cancelar</Button>
       <input {...getInputProps()} />
     </div>
