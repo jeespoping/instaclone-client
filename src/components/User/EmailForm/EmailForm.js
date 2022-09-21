7d4a28f91beef22ff605dbf32ac28bf0cd231fc7
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-09-21 12:06:33 -0500

    Actualizndo el email del usuario

--- src/components/User/EmailForm/EmailForm.js
+++ src/components/User/EmailForm/EmailForm.js
@@ -2,9 +2,13 @@ import React from "react";
 import { Form, Button } from "semantic-ui-react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import { useMutation } from "@apollo/client";
+import { UPDATE_USER } from "../../../gql/user";
 import "./EmailForm.scss";
+import { toast } from "react-toastify";
 
 export default function EmailForm({ setShowModal, currentEmail }) {
+  const [updateUser] = useMutation(UPDATE_USER);
   const formik = useFormik({
     initialValues: {
       email: currentEmail || "",
@@ -12,8 +16,19 @@ export default function EmailForm({ setShowModal, currentEmail }) {
     validateOnChange: Yup.object({
       email: Yup.string().email().required(),
     }),
-    onSubmit: (formData) => {
-      console.log(formData);
+    onSubmit: async (formData) => {
+      try {
+        await updateUser({
+          variables: {
+            input: {
+              email: formData.email,
+            },
+          },
+        });
+        setShowModal(false);
+      } catch (error) {
+        toast.error("Error al actualizar el email");
+      }
     },
   });
   return (
