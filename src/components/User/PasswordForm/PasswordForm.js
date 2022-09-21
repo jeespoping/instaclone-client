b73b9bffb209e15e4d8f2d154444f1f3bc29881f
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-09-21 10:15:56 -0500

    Añadiendo formik y yuop al formulario de cambiar contraseña

--- src/components/User/PasswordForm/PasswordForm.js
+++ src/components/User/PasswordForm/PasswordForm.js
@@ -1,13 +1,50 @@
 import React from "react";
 import { Form, Button } from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import "./PasswordForm.scss";
 
 export default function PasswordForm() {
+  const formik = useFormik({
+    initialValues: initialValuse(),
+    validationSchema: Yup.object({
+      currentPassword: Yup.string().required(),
+      newPassword: Yup.string()
+        .required()
+        .oneOf([Yup.ref("repeatNewPassword")]),
+      repeatNewPassword: Yup.string()
+        .required()
+        .oneOf([Yup.ref("currentPassword")]),
+    }),
+    onSubmit: (formValue) => {
+      console.log("Formulario enviado");
+      console.log(formValue);
+    },
+  });
+
   return (
-    <Form className="password-form">
-      <Form.Input placeholder="Contraseña actual" name="currentPassword" />
-      <Form.Input placeholder="Nueva contraseña" name="newPassword" />
+    <Form className="password-form" onSubmit={formik.handleSubmit}>
       <Form.Input
+        type="password"
+        error={formik.errors.currentPassword && true}
+        onChange={formik.handleChange}
+        value={formik.values.currentPassword}
+        placeholder="Contraseña actual"
+        name="currentPassword"
+      />
+      <Form.Input
+        type="password"
+        error={formik.errors.newPassword && true}
+        onChange={formik.handleChange}
+        value={formik.values.newPassword}
+        placeholder="Nueva contraseña"
+        name="newPassword"
+      />
+      <Form.Input
+        type="password"
+        error={formik.errors.repeatNewPassword && true}
+        onChange={formik.handleChange}
+        value={formik.values.repeatNewPassword}
         placeholder="Repetir nueva contraseña"
         name="repeatNewPassword"
       />
@@ -16,4 +53,12 @@ export default function PasswordForm() {
       </Button>
     </Form>
   );
+}
+
+function initialValuse() {
+  return {
+    currentPassword: "",
+    newPassword: "",
+    repeatNewPassword: "",
+  };
 }
