426602ddd7ccc9b0b541f8275cc24c76a72289e9
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-09-22 09:14:40 -0500

    Mostrando resultado de la busqueda

--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Search as SearchUS } from "semantic-ui-react";
+import { size } from "lodash";
 import { useQuery } from "@apollo/client";
 import { SEARCH } from "../../../gql/user";
 import "./Search.scss";
 
 export default function Search() {
   const [search, setSearch] = useState(null);
+  const [result, setResult] = useState([]);
 
   const { data, loading } = useQuery(SEARCH, {
     variables: {
@@ -13,7 +15,22 @@ export default function Search() {
     },
   });
 
-  console.log(data);
+  useEffect(() => {
+    if (size(data?.search) > 0) {
+      const users = [];
+      data.search.forEach((user, index) => {
+        users.push({
+          key: index,
+          title: user.name,
+          username: user.username,
+          avata: user.avatar,
+        });
+      });
+      setResult(users);
+    } else {
+      setResult([]);
+    }
+  }, [data]);
 
   const onChange = (e) => {
     if (e.target.value) setSearch(e.target.value);
@@ -25,7 +42,19 @@ export default function Search() {
       className="search-users"
       fluid
       input={{ icon: "search", iconPosition: "left" }}
+      loading={loading}
+      value={search || ""}
+      results={result}
+      resultRenderer={(e) => <ResultSearch data={e} />}
       onSearchChange={onChange}
     />
+  );
+}
+
+function ResultSearch({ data }) {
+  return (
+    <div>
+      <h2>Hola mundo</h2>
+    </div>
   );
 }
