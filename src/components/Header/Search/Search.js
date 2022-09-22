58d53c890c457671edec9ed194b546ff4afd8564
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-09-22 09:05:55 -0500

    Ejecutando la query en el buscador

--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Search as SearchUS } from "semantic-ui-react";
+import { useQuery } from "@apollo/client";
+import { SEARCH } from "../../../gql/user";
 import "./Search.scss";
 
 export default function Search() {
+  const [search, setSearch] = useState(null);
+
+  const { data, loading } = useQuery(SEARCH, {
+    variables: {
+      search,
+    },
+  });
+
+  console.log(data);
+
+  const onChange = (e) => {
+    if (e.target.value) setSearch(e.target.value);
+    else setSearch(null);
+  };
+
   return (
     <SearchUS
       className="search-users"
       fluid
       input={{ icon: "search", iconPosition: "left" }}
+      onSearchChange={onChange}
     />
   );
 }
