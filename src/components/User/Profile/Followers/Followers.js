c498ccd012b4fcd27dbcfad7b3139f8c8b8962e4
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-09-22 12:07:48 -0500

    Abriendo modal al hacer click a los seguidores

--- src/components/User/Profile/Followers/Followers.js
+++ src/components/User/Profile/Followers/Followers.js
@@ -1,10 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
 import { GET_FOLLOWERS } from "../../../../gql/follow";
 import "./Followers.scss";
 import { size } from "lodash";
+import ModalBasic from "../../../Modal/ModalBasic";
 
 export default function Followers({ username }) {
+  const [showmodal, setShowmodal] = useState(false);
+  const [titleModal, setTitleModal] = useState("");
+  const [childrenModal, setChildrenModal] = useState(null);
+
   const {
     data: dataFollowers,
     loading: loadingFollowers,
@@ -23,19 +28,34 @@ export default function Followers({ username }) {
     };
   }, [startPollingFollowers, stopPollingFollowers]);
 
+  const openFollowers = () => {
+    setTitleModal("Seguidores");
+    setChildrenModal(
+      <div>
+        <h3>Lista de seguidores</h3>
+      </div>
+    );
+    setShowmodal(true);
+  };
+
   if (loadingFollowers) return null;
 
   return (
-    <div className="followers">
-      <p>
-        <span>**</span> publicaciones
-      </p>
-      <p className="link">
-        <span>{size(dataFollowers.getFollowers)}</span> seguidores
-      </p>
-      <p className="link">
-        <span>**</span> seguidos
-      </p>
-    </div>
+    <>
+      <div className="followers">
+        <p>
+          <span>**</span> publicaciones
+        </p>
+        <p className="link" onClick={openFollowers}>
+          <span>{size(dataFollowers.getFollowers)}</span> seguidores
+        </p>
+        <p className="link">
+          <span>**</span> seguidos
+        </p>
+      </div>
+      <ModalBasic show={showmodal} setShow={setShowmodal} title={titleModal}>
+        <h2>Usuario</h2>
+      </ModalBasic>
+    </>
   );
 }
