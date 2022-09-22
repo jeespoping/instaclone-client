9bc0caba2c35e9447dda2528e21a4939d22fb4cf
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-09-22 10:59:19 -0500

    Boton pra dejar de seguir usuario

--- src/components/User/Profile/HeaderProfile/HeaderProfile.js
+++ src/components/User/Profile/HeaderProfile/HeaderProfile.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Button } from "semantic-ui-react";
 import { useQuery, useMutation } from "@apollo/client";
-import { IS_FOLLOW, FOLLOW } from "../../../../gql/follow";
+import { IS_FOLLOW, FOLLOW, UN_FOLLOW } from "../../../../gql/follow";
 import "./HeaderProfile.scss";
 
 export default function HeaderProfile({ getUser, auth, handlerModal }) {
   const [follow] = useMutation(FOLLOW);
+  const [unFollow] = useMutation(UN_FOLLOW);
 
   const { data, loading, refetch } = useQuery(IS_FOLLOW, {
     variables: {
@@ -15,7 +16,11 @@ export default function HeaderProfile({ getUser, auth, handlerModal }) {
 
   const buttonFollow = () => {
     if (data.isFollow) {
-      return <Button className="btn-danger">Dejar de seguir</Button>;
+      return (
+        <Button onClick={onUnFollow} className="btn-danger">
+          Dejar de seguir
+        </Button>
+      );
     } else {
       return (
         <Button onClick={onFollow} className="btn-action">
@@ -28,6 +33,19 @@ export default function HeaderProfile({ getUser, auth, handlerModal }) {
   const onFollow = async () => {
     try {
       await follow({
+        variables: {
+          username: getUser.username,
+        },
+      });
+      refetch();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onUnFollow = async () => {
+    try {
+      await unFollow({
         variables: {
           username: getUser.username,
         },
