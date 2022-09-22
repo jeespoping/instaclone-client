12115e3a9203b3faabd960e8677f8cbf9f88e041
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-09-22 12:28:41 -0500

    Evento para ir al perfil

--- src/components/User/ListUsers/ListUsers.js
+++ src/components/User/ListUsers/ListUsers.js
@@ -2,16 +2,28 @@ import React from "react";
 import { Image } from "semantic-ui-react";
 import ImageNoFound from "../../../assets/png/avatar.png";
 import { map, size } from "lodash";
+import { useHistory } from "react-router-dom";
 import "./ListUsers.scss";
 
 export default function ListUsers({ setShowmodal, users }) {
+  const history = useHistory();
+
+  const goToUser = (username) => {
+    setShowmodal(false);
+    history.push(`/${username}`);
+  };
+
   return (
     <div className="list-users">
       {size(users) === 0 ? (
         <p className="list-users__not-users">No se han encontrado usuarios</p>
       ) : (
         map(users, (user, index) => (
-          <div className="list-users__user" key={index}>
+          <div
+            onClick={() => goToUser(user.username)}
+            className="list-users__user"
+            key={index}
+          >
             <Image src={user.avatar || ImageNoFound} avatar />
             <div>
               <p>{user.name}</p>
