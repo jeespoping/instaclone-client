71378021c7814bea5f4ec06569db96bd1987211a
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-09-24 10:49:22 -0500

    Validando formulario de comentarios

--- src/components/Modal/ModalPublication/CommentForm/CommentForm.js
+++ src/components/Modal/ModalPublication/CommentForm/CommentForm.js
@@ -1,11 +1,32 @@
 import React from "react";
 import { Form, Button } from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import "./CommentForm.scss";
+import { validateSchema } from "graphql";
 
 export default function CommentForm({ publiction }) {
+  const formik = useFormik({
+    initialValues: {
+      comment: "",
+    },
+    validateSchema: Yup.object({
+      comment: Yup.string().required(),
+    }),
+    onSubmit: (formData) => {
+      console.log(formData);
+    },
+  });
+
   return (
-    <Form className="comment-form">
-      <Form.Input placeholder="Añade un comentario..." name="comment" />
+    <Form className="comment-form" onSubmit={formik.handleSubmit}>
+      <Form.Input
+        value={formik.values.comment}
+        error={formik.errors.comment}
+        onChange={formik.handleChange}
+        placeholder="Añade un comentario..."
+        name="comment"
+      />
       <Button type="submit">Publicar</Button>
     </Form>
   );
