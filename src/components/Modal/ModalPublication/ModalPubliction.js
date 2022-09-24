5006d21a14f15927db83daa48a2f38d2fdd2c69f
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-09-24 09:59:04 -0500

    Mostrando la imagen en el modal

--- src/components/Modal/ModalPublication/ModalPubliction.js
+++ src/components/Modal/ModalPublication/ModalPubliction.js
@@ -12,9 +12,12 @@ export default function ModalPubliction({
   return (
     <Modal className="modal-publication" open={showModal} onClose={onClose}>
       <Grid>
-        <Grid.Column className="modal-publication__left" width={10}>
-          <h3>IMAGEN</h3>
-        </Grid.Column>
+        <Grid.Column
+          className="modal-publication__left"
+          width={10}
+          style={{ backgroundImage: `url("${publiction.file}")` }}
+        />
+
         <Grid.Column className="modal-pulication__right" width={6}>
           <h3>Comentarios</h3>
         </Grid.Column>
