69edd82dbcf6efe206dacb7d949f7e4de428fa05
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-09-24 09:48:36 -0500

    Modal para las publicaciones

---
+++ src/components/Modal/ModalPublication/ModalPubliction.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Modal, Grid } from "semantic-ui-react";
+import "./ModalPubliction.scss";
+
+export default function ModalPubliction({
+  showModal,
+  setShowModal,
+  publiction,
+}) {
+  const onClose = () => setShowModal(false);
+
+  return (
+    <Modal className="modal-publication" open={showModal} onClose={onClose}>
+      <Grid>
+        <Grid.Column className="modal-publication__left" width={10}>
+          <h3>IMAGEN</h3>
+        </Grid.Column>
+        <Grid.Column className="modal-pulication__right" width={6}>
+          <h3>Comentarios</h3>
+        </Grid.Column>
+      </Grid>
+    </Modal>
+  );
+}
--- src/components/Publications/PreviewPublication/PreviewPublication.js
+++ src/components/Publications/PreviewPublication/PreviewPublication.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Image } from "semantic-ui-react";
+import ModalPubliction from "../../Modal/ModalPublication";
 import "./PreviewPublication.scss";
 
 export default function PreviewPublication({ publiction }) {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <>
-      <div className="preview-publication">
+      <div className="preview-publication" onClick={() => setShowModal(true)}>
         <Image className="preview-publication__image" src={publiction.file} />
       </div>
+      <ModalPubliction
+        publiction={publiction}
+        showModal={showModal}
+        setShowModal={setShowModal}
+      />
     </>
   );
 }
