6d809aa25742128c7eb0caf21079001b7590c25c
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-09-26 09:13:45 -0500

    Funcion para elminar like

--- src/components/Modal/ModalPublication/Actions/Actions.js
+++ src/components/Modal/ModalPublication/Actions/Actions.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Icon } from "semantic-ui-react";
 import { useMutation, useQuery } from "@apollo/client";
-import { ADD_LIKE, IS_LIKE } from "../../../../gql/like.";
+import { ADD_LIKE, DELETE_LIKE, IS_LIKE } from "../../../../gql/like.";
 import "./Actions.scss";
 
 export default function Actions({ publication }) {
   const [addLike] = useMutation(ADD_LIKE);
+  const [deleteLike] = useMutation(DELETE_LIKE);
   const { data, loading, refetch } = useQuery(IS_LIKE, {
     variables: {
       idPublication: publication.id,
@@ -25,8 +26,17 @@ export default function Actions({ publication }) {
     }
   };
 
-  const onDeleteLike = () => {
-    console.log("eliminar like");
+  const onDeleteLike = async () => {
+    try {
+      await deleteLike({
+        variables: {
+          idPublication: publication.id,
+        },
+      });
+      refetch();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   if (loading) return null;
