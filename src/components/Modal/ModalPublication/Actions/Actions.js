14718a08ba4342b16b4d6a12a704155647e40c6e
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-09-26 09:01:25 -0500

    Añadiendo la funcionalidad de like

--- src/components/Modal/ModalPublication/Actions/Actions.js
+++ src/components/Modal/ModalPublication/Actions/Actions.js
@@ -1,11 +1,27 @@
 import React from "react";
 import { Icon } from "semantic-ui-react";
+import { useMutation } from "@apollo/client";
+import { ADD_LIKE } from "../../../../gql/like.";
 import "./Actions.scss";
 
 export default function Actions({ publication }) {
+  const [addLike] = useMutation(ADD_LIKE);
+
+  const onAddLike = async () => {
+    try {
+      await addLike({
+        variables: {
+          idPublication: publication.id,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="actions">
-      <Icon className="like" name="heart" />
+      <Icon onClick={onAddLike} className="like" name="heart" />
       27 Likes
     </div>
   );
