c4e578c6cb4f5b8043a9bd06e0e557a59aab7548
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-09-26 09:53:07 -0500

    Estructura de l home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,16 @@
 import React from "react";
+import { Grid } from "semantic-ui-react";
 import "./Home.scss";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <Grid className="home">
+      <Grid.Column className="home__left" width={11}>
+        <h2>Feed</h2>
+      </Grid.Column>
+      <Grid.Column className="home__right" width={5}>
+        <h2>Usuarios no seguidos</h2>
+      </Grid.Column>
+    </Grid>
+  );
 }
