147d4ce5196adca34fa3a1933ba399ad7bb415f4
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2023-07-26 15:50:17 -0500

    Agregando el servidor

--- src/config/apollo.js
+++ src/config/apollo.js
@@ -4,7 +4,7 @@ import { setContext } from "apollo-link-context";
 import { getToken } from "../utils/token";
 
 const httpLink = createUploadLink({
-  uri: "http://localhost:4000/graphql",
+  uri: "https://instaclone-serve-production.up.railway.app/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
